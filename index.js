5b9fa0f531b0ed33b9b71fbfca3fb0824095a7ff
Tim Shipman <bricklayertim@yahoo.com>
2017-11-12 20:30:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+let polishedBoardMember => new BoardMember("Mr. Polished", "New York", "law")
+
+function BoardMembers(name, homeState, training) {
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+}
