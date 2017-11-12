979dd725489bbf852c7aee8b329ef6dfb10ad158
Tim Shipman <bricklayertim@yahoo.com>
2017-11-12 21:42:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-let polishedBoardMember => new BoardMember("Mr. Polished", "New York", "law")
 
-function BoardMembers(name, homeState, training) {
+
+class BoardMember {
+  constructor(name, homeState, training){
     this.name = name
     this.homeState = homeState
     this.training = training
+  }
+    veto(){return `No, I must disagree`}
+
+    approve(){return `You can do that!`}
+
+    doCharity(){return `I like to help people.`}
+
+    releasePressStatement(){return `You will see great things from Scuber.`}
+
+  sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
+
 }
