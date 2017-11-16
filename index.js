03f8208d6136d4785541fb547cb34c0975b99021
AC <msashvegas@gmail.com>
2017-11-16 09:08:10 +0000

    Done.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function BoardMember(name, homeState, training) {
   }
 
   this.sayHi = function(){
-    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 
 }
