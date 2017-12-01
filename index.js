e3c14329c4fd53360b8398314fe3017c4922470b
Melissa Hummelt <mkhummelt@gmail.com>
2017-12-01 01:00:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function BoardMember (name, homeState, training) {
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
 
-  this.veto = function() {
-    return "No, I must disagree"
+  this.veto = function(){
+    return 'No, I must disagree'
   }
-  this.approve = function() {
-    return "You can do that!"
+  this.approve = function(){
+    return 'You can do that!'
   }
 
-  this.doCharity = function() {
+  this.doCharity = function(){
     return "I like to help people."
   }
 
-  this.releasePressStatement = function() {
+  this.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
 
-  this.sayHi function() {
+  this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
